8c7cd02d6620992842d5a630c49be36ce3db26ba
haifengchengguang <60420171+haifengchengguang@users.noreply.github.com>
2021-10-09 21:44:20 +0800

    2021/10/4

--- predict1.py
+++ predict1.py
@@ -11,8 +11,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    df = pd.read_csv(r"E:\学习资料\天文\作业五\desktop\lowmass_0neten.csv")
-    X = np.expand_dims(df.values[1:, 21:66].astype(float), axis=2)
+    df = pd.read_csv(r"C:\Users\Administrator\Desktop\lowmass_sdss_2mass_wise_gaia_part1_45.csv")
+    X = np.expand_dims(df.values[1:, 22:67].astype(float), axis=2)
     json_path = './class_indices.json'
     assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
 
@@ -20,17 +20,17 @@ def main():
     class_indict = json.load(json_file)
     subclass_am=21
     model=baseline_model(subclass_amount=subclass_am)
-    weighs_path = "./save_weights/myAlex_45.h5"
+    weighs_path = "./save_weights_extinc_1009/myAlex_18.h5"
     #assert os.path.exists(img_path), "file: '{}' dose not exist.".format(weighs_path)
     model.load_weights(weighs_path)
     result = np.squeeze(model.predict(X))
-    # print(result.shape)
-    # print(type(result))
+    print(result.shape)
+    print(type(result))
     # np.savetxt('testdataresult.csv',result,delimiter=',')
-    predict_class = np.argmax(result,axis=0)
-    print(predict_class.shape)
-    print(type(predict_class))
-    np.savetxt('testdataresult2.csv', predict_class, delimiter=',')
+    # predict_class = np.argmax(result,axis=0)
+    # print(predict_class.shape)
+    # print(type(predict_class))
+    np.savetxt('lowmass_predict_part1.csv', result, delimiter=',',fmt="%.5f")
     # print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_class)],
     #                                              result[predict_class])
     # plt.title(print_res)
@@ -38,5 +38,5 @@ def main():
     # plt.show()
 
 if __name__ == '__main__':
-    print('main')
+    print('start')
     main()
--- testdataresult_manage.py
+++ testdataresult_manage.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 # df = pd.read_csv(r"testdataresult_sql.csv")
 # X = np.expand_dims(df.values[0:, 0:24].astype(float), axis=1)
 # print(X)
@@ -13,23 +14,41 @@ import pandas as pd
 # csv_reader = csv.reader(open("./testdataresult_sql.csv"))
 # result=np
 # #sep 指定分隔符
-data = pd.read_table("testdataresult_sql.csv",sep=",")
+data = pd.read_table("lowmass_predict_part1.csv",sep=",")
 #print(data)
 #lenth=len(data)
-lenth=200
-result=np.empty(shape=[lenth,2],dtype=float)
-for i in range(0, lenth):
+lenth=800000
+result=np.empty(shape=[0,2],dtype=float)
+#print(result)
+result2=np.empty(shape=[0,2],dtype=float)
+for i in tqdm(range(0, lenth)):
     a=data.iloc[i]
-    maxvalue=a.max()
-    max_index=a[a.values==maxvalue]
-    max_index_str=str(max_index)
-    #print(type(max_index))
-    indexlist=max_index.index.tolist()
-    print(indexlist[0])
-    print(max_index.iloc[0])
-    result=np.append(result,[[indexlist[0],max_index.iloc[0]]],axis=0)
-    print(i)
-np.savetxt('testdataresult_subclass.csv', result, fmt="%.5f",delimiter=',')
+    #print(type(a))
+    b=a.nlargest(2)
+    index=b.index.tolist()
+    value=b.tolist()
+    #print(value[0])
+    #print(a)
+    #array1=pd.to_numeric(a.str.get(''),errors='coerce').nlargest(2,keep='all')
+    # maxvalue=a.max()
+    # max_index=a[a.values==maxvalue]
+    # max_index_str=str(max_index)
+    # #print(type(max_index))
+    # indexlist=max_index.index.tolist()
+    # print(indexlist[0])
+    # print(max_index.iloc[0])
+    result=np.append(result,[[index[0],value[0]]],axis=0)
+    result2 = np.append(result2, [[index[1], value[1]]], axis=0)
+    #print(i)
+    # #result=np.insert()
+    # print(i)
+# print(result.shape)
+# print("------------------------------------------------------------------------------------")
+# print(result2.shape)
+print("best")
+np.savetxt('lowmass_predict_part1_best.csv', result, fmt="%s", delimiter=',')
+print("second")
+np.savetxt('lowmass_predict_part1_second.csv', result2,fmt="%s",delimiter=',')
 print("end")
 
     # row_max_value=row.max()
